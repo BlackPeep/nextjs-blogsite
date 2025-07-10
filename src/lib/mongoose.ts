ba42aa08cfd7b1c1798b9f4a8d1c30a127604853
Damian <damwinkler@icloud.com>
2025-07-10 12:19:45 +0200

    fixed another bug that hindered deployment

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,23 +1,27 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URI = process.env.MONGO_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error("Please define the MONGO URI environment variable");
+declare global {
+  var mongoose: { conn: Mongoose | null; promise: Promise<Mongoose> | null };
 }
 
-const cached = (global as any).mongoose || { conn: null, promise: null };
+const globalCache = globalThis.mongoose || {
+  conn: null,
+  promise: null,
+};
 
-export async function connectToDB() {
-  if (cached.conn) return cached.conn;
+globalThis.mongoose = globalCache;
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
+export async function connectToDB(): Promise<Mongoose> {
+  if (globalCache.conn) return globalCache.conn;
+
+  if (!globalCache.promise) {
+    globalCache.promise = mongoose.connect(MONGODB_URI, {
       dbName: "nextjs-blog",
-      bufferCommands: false,
     });
   }
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+  globalCache.conn = await globalCache.promise;
+  return globalCache.conn;
 }
