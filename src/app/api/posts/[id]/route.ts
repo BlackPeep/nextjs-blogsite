b619ef7a75a3eb067fd1ec53d8a11d8227b7d8cd
Damian <damwinkler@icloud.com>
2025-07-10 12:31:37 +0200

    fixed another bug that hindered deployment

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,23 +1,26 @@
 import { connectToDB } from "@/lib/mongoose";
 import { Post } from "@/models/Post";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   await connectToDB();
-  const post = await Post.findById(params.id);
+  const post = await Post.findById(context.params.id);
   if (!post) return NextResponse.json({ error: "Not found" }, { status: 404 });
   return NextResponse.json(post);
 }
 
 export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: { id: string } }
 ) {
   await connectToDB();
   const { title, content } = await req.json();
 
   const updated = await Post.findByIdAndUpdate(
-    params.id,
+    context.params.id,
     {
       title,
       content,
@@ -33,11 +36,11 @@ export async function PUT(
 }
 
 export async function DELETE(
-  _: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: { id: string } }
 ) {
   await connectToDB();
-  const deleted = await Post.findByIdAndDelete(params.id);
+  const deleted = await Post.findByIdAndDelete(context.params.id);
   if (!deleted)
     return NextResponse.json({ error: "Not found" }, { status: 404 });
   return NextResponse.json({ message: "deleted" });
